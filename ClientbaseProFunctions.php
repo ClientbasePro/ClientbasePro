a74a94fb9686b7d98c8bcf1483815f0e0ed64158
ClientbasePro.ru <42553531+ClientbasePro@users.noreply.github.com>
2021-11-08 23:49:48 +0300

    delete Sync1CTask() and GetSelect

--- ClientbaseProFunctions.php
+++ ClientbaseProFunctions.php
@@ -414,84 +414,6 @@ function GetIPAddressData($IP='') {
 }
 
 
-    // функция обрабатывает задачу по синхронизации КБ и 1С
-    // int $someTableId - id таблицы, в которой синхронизируются данные
-    // json $someData - перечень данных к синхронизации в формате JSON, [{"GUID":"XXXXXXX","fieldId":"fieldValue"}], поиск строки для синхронизации выполняется по GUID
-    // $prefix - префикс таблиц в КБ, по умолчанию "cb_". В КБ хранится в конфиге в $config['table_prefix']
-    // int $updateLimit - лимит кол-ва обновляемых строк за 1 запуск задачи, по умолчанию 99
-    // int $insertLimit - лимит кол-ва добавляемых строк за 1 запуск задачи, по умолчанию 99
-    // $startTime - полное время обработки задачи на настоящий момент, формат ЧЧ:ММ:СС
-function Sync1CTask(int $someTableId, $someData, $prefix="cb_", $updateLimit=99, $insertLimit=99, $startTime) {    
-        // проверка необходимых входных данных
-    if (!($someTableId=intval($someTableId)) || !$someData) return false;
-        // перевод времени обработки задачи из ЧЧ:ММ:СС в секунды
-    $startTime = explode(":", $startTime);
-    $startTime = intval($startTime[2]) + 60*intval($startTime[1]) + 3600*intval($startTime[0]);
-    $start = time();
-        // массив типов полей КБ
-    $fieldTypes_ = array(
-        1 => 'integer',
-        2 => 'date',
-        3 => 'string',
-        4 => 'list',
-        5 => 'link'
-    );
-    $data = array();    
-        // 1. массив всех записей всех таблиц GUID=>ID
-    $res = sql_query("SELECT id, table_id FROM ".$prefix."fields WHERE name_field='GUID' AND id>0 AND table_id>0 AND type_field IN (1,2,3,4,5)");
-    while ($row=sql_fetch_assoc($res)) $GUIDs[$row['table_id']] = 'f'.$row['id'];
-    foreach ($GUIDs as $tableId=>$GUIDFieldId) {
-        $res = data_select_field($tableId, 'id, '.$GUIDFieldId.' AS GUID', $GUIDFieldId."!=''");
-        while ($row=sql_fetch_assoc($res)) $GUIDData[$row['GUID']] = $row['id'];    
-    }    
-        // 2. массив типов полей fieldId=>fieldType
-    $res = sql_query("SELECT id, type_field FROM ".$prefix."fields WHERE id>0 AND type_field IN (1,2,3,4,5)");
-    while ($row=sql_fetch_assoc($res)) $fieldTypes['f'.$row['id']] = $fieldTypes_[$row['type_field']];   
-        // 3. форматируем полученные данные $someData в ассоциативный массив $data (числа -> float, даты -> "Y-m-d H:i:s", поля связи -> ID связанной записи)
-    $data_ = json_decode($someData, true);
-    foreach ($data_ as $row) {
-        $tmp = array();
-        foreach ($row as $key_=>$value_) {
-            if ('GUID'==$key_) $key = $value_;
-            elseif ($ft=$fieldTypes[$key_]) {
-                if ('integer'==$ft) $tmp[$key_] = floatval($value_);
-                elseif ('date'==$ft) $tmp[$key_] = (($tmp[$key_]=date("Y-m-d H:i:s",strtotime($value_))) && '0001-01-01 00:00:00'!=$tmp[$key_]) ? $tmp[$key_] : NULL_DATETIME;
-                elseif ('link'==$ft) $tmp[$key_] = $GUIDData[$value_];
-                else $tmp[$key_] = $value_;
-            }
-            $tmp[$GUIDs[$someTableId]] = $key;
-            $data[$key] = $tmp;
-        }
-    }
-    $tmp = array();    
-        // 4. выборка записей текущей таблицы
-        // перечень полей в текущей таблице $someTableId
-    $res = sql_query("SELECT id FROM ".$prefix."fields WHERE id>0 AND type_field IN (1,2,3,4,5) AND table_id='".$someTableId."'");
-    while ($row=sql_fetch_assoc($res)) $fields[] = 'f'.$row['id'];
-        // записи текущей таблицы - проходим и собираем массив $upd для обновления, при этом удаляя из исходного $data инфо по обновляемым строкам
-    $res = data_select_field($someTableId, 'id'.(($fields)?','.implode(',',$fields):'').', '.($g=$GUIDs[$someTableId]).' AS GUID', "status=0 AND ".$g."!=''");
-    while ($row=sql_fetch_assoc($res)) {
-        foreach ($fields as $fieldId)
-            if (isset($data[$row['GUID']][$fieldId]) && $data[$row['GUID']][$fieldId]!=$row[$fieldId]) 
-                $upd[$row['id']][$fieldId] = $data[$row['GUID']][$fieldId];
-        unset($data[$row['GUID']]);
-    }     
-        // 5. массив для обновления $upd - записываем в КБ
-    $i = 0;
-    foreach ($upd as $lineId=>$fieldsData) { data_update($someTableId, EVENTS_ENABLE, $fieldsData, "id='".$lineId."' LIMIT 1"); if ($i<$updateLimit) $i++; else break; }
-        // 6. массив новых записей (остался от $data) - добавляем в КБ
-    $i = 0;
-    foreach ($data as $GUID=>$row) { data_insert($someTableId, EVENTS_ENABLE, $row); if ($i<$insertLimit) $i++; else break; }
-        // 7. вывод данных
-    $a  = array('upd'=>$upd, 'ins'=>$data);
-    if (count($upd)<=$updateLimit && count($data)<=$insertLimit) $a['done'] = 1;    // признак, что задача в целом обработана
-    $a['time'] = TimeFormat(time()-$start+$startTime);                              // время обработки в секундах
-    $a['startTime'] = TimeFormat($startTime);                                       // время начала обработки
-    $a['thisTime'] = TimeFormat(time()-$start);                                     // время завершения обработки
-    return $a;
-}
-
-
     // функция добавляет $arrayToAdd и убирает $arrayToDelete значения в поле $fieldId в таблице $tableId в записи по условиям $cond (если $cond число, то считается id=$cond)
 	// $arrayToAdd может быть строкой или массивом
 	// $arrayToDelete может быть строкой или массивом или строкой 'delete', в этом случае будет удалён массив $arrayToAdd (пережиток предыдущей версии функции)
@@ -547,7 +469,7 @@ function DeleteFiles($tableId, $fieldId, $lineId) {
   $files = explode("\r\n", $row['files']);
   foreach ($files as $file) @unlink(get_file_path($fieldId, $lineId, $file));
     // очищаем поле с файлами
-  data_update($tableId, array('f'.$fieldId=>''), "id='".$lineId."' LIMIT 1");
+  data_update($tableId, array('f'.$fieldId=>''), "id=$lineId LIMIT 1");
   return true;
 }
 
@@ -700,15 +622,6 @@ function GetArrayFromTable($tableId=0,$fields='',$cond='',$function='') {
 	return $tmp;
   }
   return false;  
-}
-
-
-    // возвращает select, построенный по данным массива $selectData с выделенным значением $someValue
-    // структура $selectData: $value=>array('color'=>$color,'short'=>$shortValue)
-function GetSelect($someValue, array $selectData) {
-  $sel = '<option value=""></option>';
-  foreach ($selectData as $option=>$data) $sel .= '<option style="background-color:'.$data['color'].';" value="'.$option.'" '.(($option==$someValue)?"selected":"").'>'.$data['short'].'</option>';
-  return $sel;
 }
 
 
